34fca8f3c8eab4d4190e21f7d8da41dfe41311a6
Ruslan329 <165943934+Ruslan329@users.noreply.github.com>
2025-05-06 20:03:35 +0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="template/css/style.css">   
+    </head>
+    <body><h1><?php ?></h1>
+                      
+        <div style=" text-align: center;">
+        <div style=" text-align: left; display: inline-block;">
+        <h1 style="text-align: center;">Регистрация</h1>
+        <div style="max-width: 400px; ">
+        <div style="width: 100%;">
+      
+        <form action="" method="POST"  class="form">
+
+        <div class="container">
+            <input type="hidden"  name="action" value = 'classes/Registration/registr'>
+            
+            <label><b>Логин</b></label>      
+            <input type="text" placeholder="" name="login">
+            <!-- timepayment -->
+            <!-- settings -->
+            <!-- marker -->                 
+            <label><b>Имя</b></label>      
+            <input type="text" placeholder="" name="name">
+            
+            <label><b>Фамилия</b></label>    
+            <input type="text" placeholder="" name="surname">
+            
+            <label><b>Отчество</b></label>      
+            <input type="text" placeholder="" name="patronymic">
+            
+            <label><b>Номер телефона</b></label>      
+            <input type="text" placeholder="" name="phone">
+            <!-- photo --> 
+            <!-- pasportphoto --> 
+            <label><b>ИНН (Идентификационный Номер Налогоплательщика)</b></label>      
+            <input type="text" placeholder="" name="inn">
+            
+            <label><b>ОГРН (Основной Государственный Регистрационный Номер)</b></label>      
+            <input type="text" placeholder="" name="patronymic">
+            <!-- oge --> 
+            <label><b>Вид деятельности</b></label>      
+            <input type="text" placeholder="" name="typeactivit">
+            <!-- status -->
+            <div class="dropdown">
+                <a class="" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                Ссылка выпадающего списка
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Действие</a></li>
+                    <li><a class="dropdown-item" href="#">Другое действие</a></li>
+                    <li><a class="dropdown-item" href="#">Что-то еще здесь</a></li>
+                </ul>
+            </div>
+            <button type="submit">Зарегистрироватся</button>  
+        </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Отмена</button>
+
+  </div>
+            </div>
+        </div>
+    </div>
+   
+        
+        
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
