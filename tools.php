34319a58de340b9e3fb2c60315f4b4f5ccbbe101
sultantoch <166203969+sultantoch@users.noreply.github.com>
2025-06-02 18:53:38 +0300

    Add files via upload

---
+++ tools.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="template/css/style.css">   
+    </head>
+    <body><h1></h1>
+                  
+
+        
+        
+        
+        
+        
+        
+        <div style=" text-align: center;">
+        <div style=" text-align: left; display: inline-block;">
+        <h1 style="text-align: center;"></h1>
+        <div style="max-width: 400px; ">
+        <div style="width: 100%;">
+      <h1>VAML</h1>
+        <form action="" method="POST"  class="form">
+
+            <div class="container" style="text-align: center;">
+           <ul class="nav flex-column">
+  <li class="nav-item">
+      <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;" "role="button" aria-disabled="true">ДОБАВИТЬ СТРАНИЦУ</a>
+  </li>
+  <li class="nav-item">
+  <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;"role="button" aria-disabled="true">ДОБАВИТЬ МЕНЮ</a>
+  </li>
+  <li class="nav-item">
+     <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;" role="button" aria-disabled="true">ИЗМЕНИТЬ/УДАЛИТЬ СТРАНИЦУ</a>
+  </li>
+  <li class="nav-item">
+     <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;" role="button" aria-disabled="true">ИЗМЕНИТЬ/УДАЛИТЬ МЕНЮ</a>
+  </li>
+  
+  
+    <li class="nav-item">
+      <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;" "role="button" aria-disabled="true">АКАУНТ</a>
+  </li>
+  <li class="nav-item">
+  <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;"role="button" aria-disabled="true">НАСТРОЙКИ</a>
+  </li>
+  <li class="nav-item">
+     <a class="btn btn-primary disabled" style="margin-bottom: 4px; width: 100%; padding: 20px;" role="button" aria-disabled="true">ТЕХНИЧЕСКАЯ ПОТДЕРЖКА</a>
+  </li>
+
+</ul>
+
+
+            
+        </div>
+
+            </div>
+        </div>
+    </div>
+   
+        
+        
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
